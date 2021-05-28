0dd155ed64aeab6458ce71722605e8a99d035319
huzaifamalik47@gmail.com <huzaifamalik47@gmail.com>
2021-05-29 03:06:34 +0500

    changes done

--- src/components/Home/Items/Items.jsx
+++ src/components/Home/Items/Items.jsx
@@ -414,12 +414,22 @@ export default function Items(props) {
                   <strong>Activity</strong>
 
                   <ul className="timeline mt-2 mb-0">
-                    <li className="timeline-item">
+                    <li
+                      className="timeline-item"
+                      style={{
+                        margin: "0rem 2rem",
+                      }}
+                    >
                       <strong>Registration Date</strong>
                       <span className="float-right text-muted text-sm"></span>
                       <p>{vendorDetails.date}</p>
                     </li>
-                    <li className="timeline-item">
+                    <li
+                      className="timeline-item"
+                      style={{
+                        margin: "0rem 2rem",
+                      }}
+                    >
                       <strong>First Review</strong>
                       <span className="float-right text-muted text-sm">
                         {reviews.slice(-1).map((v) => {
@@ -432,7 +442,12 @@ export default function Items(props) {
                         })}
                       </p>
                     </li>
-                    <li className="timeline-item">
+                    <li
+                      className="timeline-item"
+                      style={{
+                        margin: "0rem 2rem",
+                      }}
+                    >
                       <strong>Latest Review</strong>
                       <span className="float-right text-muted text-sm">
                         {reviews.slice(0, 1).map((v) => {
